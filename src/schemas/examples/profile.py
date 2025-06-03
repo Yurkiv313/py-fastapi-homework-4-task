a4539d0dc8db0d9492aca40d115ffca9cc4694ea
Andrii <yurkivandriy02@gmail.com>
2025-06-03 13:10:47 +0300

    Fix flake8

--- src/schemas/examples/profile.py
+++ src/schemas/examples/profile.py
@@ -1,8 +1,8 @@
 profile_schema_example = {
-  "first_name": "John",
-  "last_name": "Doe",
-  "gender": "man",
-  "date_of_birth": "1990-01-01",
-  "info": "This is a test profile.",
-  "avatar": "<binary-image-data>"
+    "first_name": "John",
+    "last_name": "Doe",
+    "gender": "man",
+    "date_of_birth": "1990-01-01",
+    "info": "This is a test profile.",
+    "avatar": "<binary-image-data>"
 }
